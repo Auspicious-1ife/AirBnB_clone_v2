570c9fcd4f90851625841a720fa68b33e5f8610b
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-28 17:10:26 +0100

    a script that starts a flask web application and displaysn Hello HBNB! and HBNB with the commands / and /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,7 +3,24 @@
 A script to run the Flask web application.
 """
 
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route that returns 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route that returns 'HBNB'
+    """
+    return "HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
