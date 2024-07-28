737a1bbab8cc05faaa20c4a6504eba86c100287a
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-28 16:54:01 +0100

    corrected the task 0 code by addng the route

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A simple Flask web application,that listens on 0.0.0.0,port 5000
+A simple Flask web application that listens on 0.0.0.0, port 5000
 and displays 'Hello HBNB!' on the root route.
 """
 
@@ -8,18 +8,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Route that returns 'Hello HBNB!'
     """
     return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Route that returns 'HBNB'
-    """
-    return "HBNB!"
